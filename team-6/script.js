2b1eb8fafacdf6f16e7d6e50beac627dbafac4a7
jeksnow <ztoskol@gmail.com>
2022-12-14 20:07:18 +0700

    up kata gambar

--- team-6/script.js
+++ team-6/script.js
@@ -5,8 +5,31 @@ $.getJSON('./API/Blues.json', function (data){
                 <div class="card" style="width: 18rem;">
                     <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">`+ data2.nama_SUBGENRE +`</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title">`+ data2.SUBGENRE +`</h5>
+                        <p class="card-text">`+ data2.Band +`</p>
+                        <p class="card-text">`+ data2.Album +`</p>
+                        <p class="card-text">`+ data2.Judul +`</p>
+                        <p class="card-text">`+ data2.Tahun +`</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        `);
+    });
+});
+
+$.getJSON('./API/Dangdut.json', function (data){
+    $.each(data, function(i,data2){
+        $('.kotak-lagu').append(`
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="./assets/images/`+ data2.Gambar +`" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">`+ data2.SUBGENRE +`</h5>
+                        <p class="card-text">`+ data2.Band +`</p>
+                        <p class="card-text">`+ data2.Album +`</p>
+                        <p class="card-text">`+ data2.Judul +`</p>
+                        <p class="card-text">`+ data2.Tahun +`</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
