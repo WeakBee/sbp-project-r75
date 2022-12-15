4c64eaaf68bd27a1f2308317959d917ec75a3ad5
Agung-wsi <110399315+Agung-wsi@users.noreply.github.com>
2022-12-14 18:18:11 -0800

    update

--- team-7/informasi/script2.js
+++ team-7/informasi/script2.js
@@ -1,6 +1,6 @@
-$.getJSON('../API/mamalia.json', function (data){
+$.getJSON('../API/burung.json', function (data){
     $.each(data, function(i,data2){
-        $('.mamalia').append(`
+        $('.burung').append(`
             <div class="col-4">
                 <div class="card">
                     <img src="`+ data2.Gambar +`" class="card-img-top" alt="...">
@@ -11,8 +11,8 @@ $.getJSON('../API/mamalia.json', function (data){
                         <p class="card-text">Lokasi : `+ data2.Lokasi +`</p>
                         <p class="card-text">Ukuran Tubuh : `+ data2.UkuranTubuh +`</p>
                         <p class="card-text">Panjang Ekor : `+ data2.PanjangEkor +`</p>
-                        <p class="card-text">Berat : `+ data2.Berat +`</p>
-                        <p class="card-text">Golongan : `+ data2.Golongan +`</p>
+                        <p class="card-text">Bentuk Paruh: `+ data2.BentukParuh +`</p>
+                        <p class="card-text">Golongan : `+ data2.Makanan +`</p>
                         <p class="card-text">Tipe Kulit : `+ data2.TipeKulit +`</p>
                         <p class="card-text">Warna : `+ data2.Warna +`</p>
                         </div>
