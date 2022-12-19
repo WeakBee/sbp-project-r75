e707bc27c971041a44475d7f3b870a62bd462a15
vousdnanez <hwangna17@gmail.com>
2022-12-19 14:01:58 +0700

    update gatau dah

--- team-3/script.js
+++ team-3/script.js
@@ -1,6 +1,13 @@
-$.getJSON('API/apple.json', function(data) {
-    $.each(data, function(i, d){
+$.getJSON('API/apple.json', function (data) {
+    $.each(data, function (i, d) {
         console.log(data);
-        $('#apple-list').append('<div class="card" style="width: 18rem;"><img src="./assets/xiaomi/xiaomi-redmi-note-12-pro-plus.jpg" class="card-img-top"><div class="card-body"><h5 class="card-title">'+ d.Brand +'</h5><p class="card-text">'+ d.Tahun +'</p><a href="#" class="btn btn-primary">Lihat Selengkapnya</a></div></div>')
+        $('#apple-list').append(`
+        <div class="col-lg-4" style="width: 18rem;">
+            <img src="./assets/xiaomi/xiaomi-redmi-note-12-pro-plus.jpg" class="card-img-top">
+                <div class="card-body">
+                     <h5 class="card-title">`+ d.Brand +`</h5>
+                    <p class="card-text">`+ d.Tahun +`</p><a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+         </div>`)
     });
 });
