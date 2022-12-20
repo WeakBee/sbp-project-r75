d7b4dcd314030061b5d28cc33e1af6227a9b8082
Ichwanul Muslim <82445431+WeakBee@users.noreply.github.com>
2022-12-20 21:44:21 +0700

    update team 6

---
+++ team-6/blues/script.js
@@ -0,0 +1,27 @@
+// Unique
+function unique(list) {
+    var result = [];
+    $.each(list, function(i, e) {
+        if ($.inArray(e, result) == -1) result.push(e);
+    });
+    return result;
+}
+// Unique
+let subgenre = [];
+
+$.getJSON('../API/Blues.json', function (data){
+    // Membuat Kartu
+    $.each(data, function(i,data2){
+        subgenre.push(data2.SUBGENRE);
+    });
+    // Membuat Kartu
+});
+
+setTimeout(function() {
+    let subgenrefilter = unique(subgenre);
+    $.each(subgenrefilter, function(i,data){
+        $('.link-subgenre').append(`
+                <li><a href="#">`+ data +`</a></li>
+            `);
+    });
+}, 500);
